baf2796a35c10f6d9b2b79ab7e5e6686c4b26d0c
Robert McGuinness <rob.mcguinness@availity.com>
2015-03-10 22:08:05 -0400

    update docs

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -45,6 +45,23 @@ gulp.task('build:handlebars:partials', function() {
   Handlebars.registerHelper('date', function() {
     return moment().format('YYYY');
   });
+
+  // http://funkjedi.com/technology/412-every-nth-item-in-handlebars/
+  Handlebars.registerHelper('grouped_each', function(every, context, options) {
+    var out = "", subcontext = [], i;
+    if(context && context.length > 0) {
+      for(i = 0; i < context.length; i++) {
+        if(i > 0 && i % every === 0) {
+            out += options.fn(subcontext);
+            subcontext = [];
+        }
+        subcontext.push(context[i]);
+      }
+      out += options.fn(subcontext);
+    }
+    return out;
+  });
+
 });
 
 gulp.task('build:docs', function() {
