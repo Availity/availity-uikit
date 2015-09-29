2f031e65c7a0fb92ecebf03135626bdd0e6c859e
Rob McGuinness <rob.mcguinness@availity.com>
2015-09-29 03:15:38 -0400

    remove typeahead from docs example

--- docs/guide/js/docs.js
+++ docs/guide/js/docs.js
@@ -100,46 +100,6 @@
     }
   }, 2000);
 
-  // Twitter Typeahead
-  var substringMatcher = function(strs) {
-    return function findMatches(q, cb) {
-      var matches;
-      var substrRegex;
-
-      matches = [];
-      substrRegex = new RegExp(q, 'i');
-      $.each(strs, function(i, str) {
-        if (substrRegex.test(str)) {
-          matches.push({ value: str });
-        }
-      });
-      cb(matches);
-    };
-  };
-
-  var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-  ];
-
-
-  $('.form-group .form-control-suggest').typeahead({
-      hint: true,
-      highlight: true,
-      minLength: 1
-    },
-    {
-      name: 'states',
-      displayKey: 'value',
-      source: substringMatcher(states)
-  });
-
   var data = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
