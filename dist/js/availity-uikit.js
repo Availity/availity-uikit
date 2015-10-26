086192530bb08ba89d5b0c2100f95a36c0873463
Rob McGuinness <rob.mcguinness@availity.com>
2015-10-26 11:32:55 -0400

    bump package version v0.15.0

--- dist/js/availity-uikit.js
+++ dist/js/availity-uikit.js
@@ -1,30 +1,33 @@
 /**
- * availity-uikit v0.14.0 -- October-06
+ * availity-uikit v0.15.0 -- October-26
  * Copyright 2015 Availity, LLC 
  */
 
+/* global jQuery */
 /**
  * Monkey patch Bootstrap Modal and prevent shifting of a fixed top navbar when the modal
  * window is shown.
  */
 (function($) {
 
+  'use strict';
+
   var exists = $.fn.modal && $.fn.modal.Constructor;
-  if(!exists) {
+  if (!exists) {
     return;
   }
 
   var _setScrollBar = $.fn.modal.Constructor.prototype.setScrollbar;
 
-  $.fn.modal.Constructor.prototype.setScrollbar = function () {
+  $.fn.modal.Constructor.prototype.setScrollbar = function() {
     _setScrollBar.apply(this);
-    if(this.bodyIsOverflowing) {
+    if (this.bodyIsOverflowing) {
       $('.navbar-fixed-top').css('padding-right', this.scrollbarWidth);
     }
   };
 
   var _resetScrollbar = $.fn.modal.Constructor.prototype.resetScrollbar;
-  $.fn.modal.Constructor.prototype.resetScrollbar = function () {
+  $.fn.modal.Constructor.prototype.resetScrollbar = function() {
     _resetScrollbar.apply(this);
     $('.navbar-fixed-top').css('padding-right', '');
   };
