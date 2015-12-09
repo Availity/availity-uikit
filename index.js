0bfeed6d6c934f13828661a41b5e7d471a7c8df6
Rob McGuinness <rob.mcguinness@availity.com>
2015-12-08 22:22:06 -0500

    import webpack dependencies

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-require('./js/availity-modal.js');
+// LESS
+import './less/availity-bootstrap.less';
+
+// IMAGES
+import './images/logo-availity.png';
+import './images/default/love.jpg';
+
+// JS
+import './js/availity-uikit';
+
