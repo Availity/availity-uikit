2711d925523758946807bd239e7d168d3661ba13
Rob McGuinness <rob.mcguinness@availity.com>
2015-12-29 15:23:51 -0500

    remove images from js bootstrap file

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // LESS
 import './less/availity-bootstrap.less';
 
-// IMAGES
-import './images/logo-availity.png';
-import './images/default/love.jpg';
-
 // FONTS
 import './fonts/availity-icon-font.eot';
 import './fonts/availity-icon-font.svg';
