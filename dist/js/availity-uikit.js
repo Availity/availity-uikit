67cdc8196b1895f0d0755a5fbecf0bea6600cf32
Rob McGuinness <rob.mcguinness@availity.com>
2016-01-19 17:13:06 -0500

    v2.0.0-beta.1

--- dist/js/availity-uikit.js
+++ dist/js/availity-uikit.js
@@ -1,6 +1,6 @@
 /*!
  * 
- * availity-uikit v2.0.0-beta.0 (01/19/2016)
+ * availity-uikit v2.0.0-beta.1 (01/19/2016)
  * (c) Availity, LLC
  */
 (function webpackUniversalModuleDefinition(root, factory) {
