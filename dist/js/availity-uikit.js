bcb939ad9ce1617558e92a6ee9b0e30a61ae0a86
Rob McGuinness <rob.mcguinness@availity.com>
2016-07-19 22:34:54 -0400

    v2.0.0-beta.7

--- dist/js/availity-uikit.js
+++ dist/js/availity-uikit.js
@@ -1,6 +1,6 @@
 /*!
  * 
- * availity-uikit v2.0.0-beta.7 (07/13/2016)
+ * availity-uikit v2.0.0-beta.7 (07/19/2016)
  * (c) Availity, LLC
  */
 (function webpackUniversalModuleDefinition(root, factory) {
@@ -59,48 +59,13 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	__webpack_require__(1);
-	
-	__webpack_require__(2);
+	'use strict';__webpack_require__(1);__webpack_require__(2);
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _jquery = __webpack_require__(3);
-	
-	var _jquery2 = _interopRequireDefault(_jquery);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var exists = _jquery2.default.fn.modal && _jquery2.default.fn.modal.Constructor;
-	
-	function override() {
-	
-	  var _setScrollBar = _jquery2.default.fn.modal.Constructor.prototype.setScrollbar;
-	  _jquery2.default.fn.modal.Constructor.prototype.setScrollbar = function () {
-	
-	    _setScrollBar.apply(this);
-	    if (this.bodyIsOverflowing) {
-	      (0, _jquery2.default)('.navbar-fixed-top').css('padding-right', this.scrollbarWidth);
-	    }
-	  };
-	
-	  var _resetScrollbar = _jquery2.default.fn.modal.Constructor.prototype.resetScrollbar;
-	  _jquery2.default.fn.modal.Constructor.prototype.resetScrollbar = function () {
-	
-	    _resetScrollbar.apply(this);
-	    (0, _jquery2.default)('.navbar-fixed-top').css('padding-right', '');
-	  };
-	}
-	
-	if (exists) {
-	  override();
-	}
+	'use strict';var _jquery=__webpack_require__(3);var _jquery2=_interopRequireDefault(_jquery);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}var exists=_jquery2.default.fn.modal&&_jquery2.default.fn.modal.Constructor;function override(){var _setScrollBar=_jquery2.default.fn.modal.Constructor.prototype.setScrollbar;_jquery2.default.fn.modal.Constructor.prototype.setScrollbar=function(){_setScrollBar.apply(this);if(this.bodyIsOverflowing){(0,_jquery2.default)('.navbar-fixed-top').css('padding-right',this.scrollbarWidth);}};var _resetScrollbar=_jquery2.default.fn.modal.Constructor.prototype.resetScrollbar;_jquery2.default.fn.modal.Constructor.prototype.resetScrollbar=function(){_resetScrollbar.apply(this);(0,_jquery2.default)('.navbar-fixed-top').css('padding-right','');};}if(exists){override();}
 
 /***/ },
 /* 2 */
