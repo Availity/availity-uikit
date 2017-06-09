e091f37f42ef6b066f90a7083f7c739c7bfae26b
Rob McGuinness <rob.mcguinness@availity.com>
2017-06-09 10:09:45 -0400

    fix webpack build path for docs

--- webpack.config.docs.js
+++ webpack.config.docs.js
@@ -1,10 +1,11 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const webpackConfig = require('./webpack.config');
 
 const config = merge(webpackConfig, {
   output: {
     // do not add slash else => Error: EACCES: permission denied, mkdir '/build'
-    path: 'build'
+    path: path.join(__dirname, 'build')
   }
 });
 
