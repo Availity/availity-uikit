c2b4f18b9d627ae68a83391e265786f847ba8675
Rob McGuinness <rob.mcguinness@availity.com>
2017-10-26 14:19:57 -0400

    refactor(docs): redesign navigation header

--- packages/docs/src/components/Navbar.js
+++ packages/docs/src/components/Navbar.js
@@ -2,32 +2,23 @@ import React from 'react';
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Navbar</a>
+
+    <span className="navbar-brand mb-0 h1">Availity UIKit</span>
+
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon" />
     </button>
 
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          <a className="nav-link" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">Action</a>
-            <a className="dropdown-item" href="#">Another action</a>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="#">Something else here</a>
-          </div>
+          <a className="nav-link" href="#">Components</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#">Disabled</a>
+          <a className="nav-link" href="#">Github</a>
         </li>
       </ul>
     </div>
