566603dc379fee1a2fb37e185289bdf9c885420d
Rob McGuinness <rob.mcguinness@availity.com>
2017-11-20 11:11:42 -0500

    fix(docs): group components by category

--- packages/docs/src/pages/components.js
+++ packages/docs/src/pages/components.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
+import groupBy from 'lodash.groupby';
+import map from 'lodash.map';
+import slugify from 'slugify';
 import 'holderjs';
 
 const Component = ({ html, title }) => (
   <div>
     <h3>{title}</h3>
-    <div>{ReactHtmlParser(html)}</div>
+    <div className="docs-example"> {ReactHtmlParser(html)}</div>
   </div>
 );
+
+const ComponentGroup = ({ groupName, components }) => {
+  const componentsList = map(components, component => (
+    <Component
+      key={slugify(component.node.frontmatter.title)}
+      title={component.node.frontmatter.title}
+      html={component.node.html}
+    />
+  ));
+
+  return (
+    <div>
+      <h2 className="docs-title">{groupName}</h2>
+      {componentsList}
+    </div>
+  );
+};
 
 const ComponentsPage = ({ data }) => {
   const components = data.allMarkdownRemark.edges;
 
-  /* eslint-disable react/no-array-index-key */
-  const componentsList = components.map((component, i) => (
-    <Component key={i} title={component.node.frontmatter.title} html={component.node.html} />
+  // Group by frontmatter category property
+  const componentsGrouped = groupBy(components, component => component.node.frontmatter.category);
+  // Generate component group sections
+  const componntsGroupList = map(componentsGrouped, (components, componentGroupName) => (
+    <ComponentGroup key={slugify(componentGroupName)} groupName={componentGroupName} components={components} />
   ));
 
   return (
     <main className="docs-masthead">
       <h1 className="sr-only">Components Page</h1>
-      <div className="container">{componentsList}</div>
+      <div className="container">
+        <div>{componntsGroupList}</div>
+      </div>
     </main>
   );
 };
